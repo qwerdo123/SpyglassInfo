8c65f26e555df005dc263e67f9a71d019f83104e
qwerdo123 <qwerdo123@gmail.com>
2025-04-06 20:23:39 +0200

    Initial Release

---
+++ SpyGlassInfo/SpyGlassInfoModSystem.cs
@@ -0,0 +1,53 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+using HarmonyLib;
+using System.Reflection;
+using System;
+using Vintagestory.Server;
+using System.Runtime.CompilerServices;
+using spyglass.src;
+namespace SpyGlassInfo
+{
+    [HarmonyPatch]
+    public class SpyGlassInfoModSystem : ModSystem
+    {
+        public static ICoreAPI api;
+        public Harmony harmony;
+        static float previousPickingRange = 0f;
+        public override void Start(ICoreAPI api)
+        {
+            SpyGlassInfoModSystem.api = api;
+            if (!Harmony.HasAnyPatches("SpyGlassInfo"))
+            {
+                harmony = new Harmony("SpyGlassInfo");
+                harmony.PatchAll();
+            }
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch("spyglass.src.ItemSpyglass", "OnHeldInteractStart")]
+        public static void IncreaseRange(EntityAgent byEntity)
+        {
+            api.Logger.Notification("debug start");
+            if (byEntity is not EntityPlayer playerEntity) return;
+            previousPickingRange = playerEntity.Player.WorldData.PickingRange;
+            playerEntity.Player.WorldData.PickingRange = 1000;
+
+
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch("spyglass.src.ItemSpyglass", "OnHeldInteractStop")]
+        public static void ResetRange(EntityAgent byEntity)
+        {
+            api.Logger.Notification("debug stop");
+            if (byEntity is not EntityPlayer playerEntity) return;
+
+            playerEntity.Player.WorldData.PickingRange = previousPickingRange;
+
+
+        }
+    }
+}
